22171eb86ca4241287d2bdbc0339c303de6fa08f
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2014-12-05 21:25:14 +0330

    Going to Cloud for first test.

--- hge/network/Handler.py
+++ hge/network/Handler.py
@@ -4,18 +4,10 @@ import threading
 
 
 class Handler(socketserver.BaseRequestHandler):
-    def __init__(self, request, client_address, server):
-        socketserver.BaseRequestHandler.__init__(self, request, client_address, server)
-        self.data = None
-
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        cur_thread = threading.current_thread()
-        response = "{}: {}".format(cur_thread.name, self.data)
-        print("Server: {} wrote.".format(self.client_address[0]))
-        print("Server: response is {}.".format(response))
-        print(self.data)
-        self.request.sendall(self.data.upper() + bytes(response, "utf-8"))
+        data = self.request.recv(1024).strip()
+        print("Server: ", data)
+        self.request.sendall(data)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
--- hge/network/Server.py
+++ hge/network/Server.py
@@ -1,6 +1,5 @@
 __author__ = 'Hossein Noroozpour'
 import socketserver
-import threading
 from hge.network.Handler import Handler
 import socket
 
@@ -10,25 +9,23 @@ class Server(socketserver.ForkingMixIn, socketserver.TCPServer):
 
 
 if '__main__' == __name__:
-    HOST, PORT = "localhost", 0
-    server = Server((HOST, PORT), Handler)
-    ip, port = server.server_address
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
-    print("Server loop running in thread:", server_thread.name)
-
-    def client(server_ip, server_port, message):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((server_ip, server_port))
-        try:
-            sock.sendall(bytes(message, "utf-8"))
-            response = str(sock.recv(1024))
-            print("Received: {}".format(response))
-        finally:
-            sock.close()
-
-    client(ip, port, "Hello World 1")
-    client(ip, port, "Hello World 2")
-    client(ip, port, "Hello World 3")
-    server.shutdown()
+    import os
+    ip, port = "localhost", 8888
+    pid = os.fork()
+    if 0 != pid:
+        server = Server((ip, port), Handler)
+        ip, port = server.server_address
+        server.serve_forever()
+    else:
+        def client(server_ip, server_port, message):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((server_ip, server_port))
+            try:
+                sock.sendall(bytes(message, "utf-8"))
+                response = str(sock.recv(1024))
+                print("Client: {}".format(response))
+            finally:
+                sock.close()
+        client(ip, port, "Hello World 1")
+        client(ip, port, "Hello World 2")
+        client(ip, port, "Hello World 3")
